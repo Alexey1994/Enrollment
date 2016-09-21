e47759dae4bd9d7940cce30db832d4659a30979b
argo-qu <afilipovich.qu@gmail.com>
2016-09-21 08:46:00 +0300

    debugging relations

--- js/controllers/relationsController.js
+++ js/controllers/relationsController.js
@@ -2,24 +2,35 @@ app.controller('relationsController', function($scope, $rootScope) {
 	$rootScope.activePage = "relations";
 	$scope.relations = ($rootScope.relations) ? $rootScope.relations : [];
 
-	if (!$rootScope.loaded.relations) {
-		$rootScope.load("/relations");		
-		$scope.$on('dataLoaded', function(event, relations) {
-			console.log('relations', JSON.parse(JSON.stringify(relations)));
-			$rootScope.relations = JSON.parse(JSON.stringify(relations));
+	var dataLoadedCallback = function(event, data) {
+		if (data instanceof Array && 
+			relations[0].hasOwnProperty('language') &&
+			relations[0].hasOwnProperty('name') &&
+			relations[0].hasOwnProperty('prefix') &&
+			relations[0].hasOwnProperty('type')) 
+		{
+			// Значит, это пришли экзамены
+			console.log('exams', JSON.parse(JSON.stringify(data)));
+			$rootScope.exams = JSON.parse(JSON.stringify(data));
+			$scope.exams = $rootScope.exams;
+			$rootScope.loaded.exams = true;
+		} else if (data instanceof Array) {
+			// Значит пришли relations
+			console.log('relations', JSON.parse(JSON.stringify(data)));
+			$rootScope.relations = JSON.parse(JSON.stringify(data));
 			$scope.relations = $rootScope.relations;
 			$rootScope.loaded.relations = true;
+		}
+	}
 
-			if (!$rootScope.loaded.exams) {
-				$rootScope.load("/exams");
-				$scope.$on('dataLoaded', function(event, exams) {
-					console.log('exams', JSON.parse(JSON.stringify(exams)));
-					$rootScope.exams = JSON.parse(JSON.stringify(exams));
-					$scope.exams = $rootScope.exams;
-					$rootScope.loaded.exams = true;					
-				})
-			}			
-		});		
+	if (!$rootScope.loaded.relations) {
+		$rootScope.load("/relations");
+		$scope.$on('dataLoaded', dataLoadedCallback);
+	}	
+
+	if (!$rootScope.loaded.exams) {
+		$rootScope.load("/exams");
+		$scope.$on('dataLoaded', dataLoadedCallback);
 	}
 
 	$scope.newRelation = [];
